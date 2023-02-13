963c73642b9e9947c66d175719e1a372ce13e973
Yeşim <121230049+yesimagar@users.noreply.github.com>
2023-02-13 15:43:06 +0300

    Create ODEV11.sql

---
+++ ODEV11.sql
@@ -0,0 +1,36 @@
+--actor ve customer tablolarında bulunan first_name sütunları için tüm verileri sıralayalım.
+
+(SELECT first_name FROM actor)
+UNION
+(SELECT first_name FROM customer)
+ORDER BY first_name;
+
+--Tekrar eden veriler için
+
+(SELECT first_name FROM actor)
+UNION ALL
+(SELECT first_name FROM customer)
+ORDER BY first_name;
+
+
+--actor ve customer tablolarında bulunan first_name sütunları için kesişen verileri sıralayalım.
+
+(SELECT first_name FROM actor)
+INTERSECT
+(SELECT first_name FROM customer)
+ORDER BY first_name;
+
+
+--actor ve customer tablolarında bulunan first_name sütunları için ilk tabloda bulunan ancak ikinci tabloda bulunmayan verileri sıralayalım.
+
+(SELECT first_name FROM actor)
+EXCEPT
+(SELECT first_name FROM customer)
+ORDER BY first_name;
+
+--Tekrar eden veriler için
+
+(SELECT first_name FROM actor)
+EXCEPT ALL
+(SELECT first_name FROM customer)
+ORDER BY first_name;
